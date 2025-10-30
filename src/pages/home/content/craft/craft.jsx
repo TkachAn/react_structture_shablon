7ffdc3a82df90099248e33badb4d06d3b8674898
TkachAn <61586895+TkachAn@users.noreply.github.com>
2025-10-30 10:02:04 +0200

    craft content

--- src/pages/home/content/craft/craft.jsx
+++ src/pages/home/content/craft/craft.jsx
@@ -1,36 +1,38 @@
-export const Craft = () => {    
-    return (
-      <ul class="container craft__gallery">
-        <li class="craft__item">
-          <img
-            src="./images/1/img1.jpg"
-            alt="рабочее место разработчика ПО 1"
-            width="370"
-          />
-          <div class="craft__bg-title">
-            <p class="craft__img-title">Десктопные приложения</p>
-          </div>
-        </li>
-        <li class="craft__item">
-          <img
-            src="./images/1/img2.jpg"
-            alt="рабочее место разработчика ПО 2"
-            width="370"
-          />
-          <div class="craft__bg-title">
-            <p class="craft__img-title">Мобильные приложения</p>
-          </div>
-        </li>
-        <li class="craft__item">
-          <img
-            src="./images/1/img3.jpg"
-            alt="рабочее место разработчика ПО 3"
-            width="370"
-          />
-          <div class="craft__bg-title">
-            <p class="craft__img-title">Дизайнерские решения</p>
-          </div>
-        </li>
-      </ul>
-    );
+import s from "./s.module.css";
+
+export const Craft = () => {
+  return (
+    <ul className={s.gallery}>
+      <li className={s.item}>
+        <img
+          src="./images/img1.jpg"
+          alt="рабочее место разработчика ПО 1"
+          width="370"
+        />
+        <div className={s.bgTitle}>
+          <p className={s.imgTitle}>Десктопные приложения</p>
+        </div>
+      </li>
+      <li className={s.item}>
+        <img
+          src="./images/img2.jpg"
+          alt="рабочее место разработчика ПО 2"
+          width="370"
+        />
+        <div className={s.bgTitle}>
+          <p className={s.imgTitle}>Мобильные приложения</p>
+        </div>
+      </li>
+      <li className={s.item}>
+        <img
+          src="./images/img3.jpg"
+          alt="рабочее место разработчика ПО 3"
+          width="370"
+        />
+        <div className={s.bgTitle}>
+          <p className={s.imgTitle}>Дизайнерские решения</p>
+        </div>
+      </li>
+    </ul>
+  );
 };
